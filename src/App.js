b1b4c526d8f13bf38c8cd1bf34d7a8e88177f007
ashkan <ashkan16ma96@gmail.com>
2023-03-22 07:22:47 -0700

    add new product to products list

--- src/App.js
+++ src/App.js
@@ -3,50 +3,53 @@ import CategoryForm from "./Component/CategoryForm";
 import NavBar from "./Component/NavBar";
 import ProductForm from "./Component/ProductForm";
 
-const products = [
-  {
-    id: 1,
-    title: "html",
-    category: "frontend",
-    createdAt: "2021-11-03",
-  },
-  {
-    id: 2,
-    title: "css",
-    category: "frontend",
-    createdAt: "2021-10-01",
-  },
-  {
-    id: 3,
-    title: "php",
-    category: "backend",
-    createdAt: "2022-01-01",
-  },
-  {
-    id: 4,
-    title: "node.js",
-    category: "backend",
-    createdAt: "2022-04-03",
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     title: "html",
+//     category: "frontend",
+//     createdAt: "2021-11-03",
+//   },
+//   {
+//     id: 2,
+//     title: "css",
+//     category: "frontend",
+//     createdAt: "2021-10-01",
+//   },
+//   {
+//     id: 3,
+//     title: "php",
+//     category: "backend",
+//     createdAt: "2022-01-01",
+//   },
+//   {
+//     id: 4,
+//     title: "node.js",
+//     category: "backend",
+//     createdAt: "2022-04-03",
+//   },
+// ];
 
-const categories = [
-  {
-    id: 1,
-    title: "frontend",
-    description: "frontEnd applications",
-    createdAt: "2021-03-02",
-  },
-  {
-    id: 1,
-    title: "backend",
-    description: "backEnd applications",
-    createdAt: "2022-08-03",
-  },
-];
+// const categories = [
+//   {
+//     id: 1,
+//     title: "frontend",
+//     description: "frontEnd applications",
+//     createdAt: "2021-03-02",
+//   },
+//   {
+//     id: 1,
+//     title: "backend",
+//     description: "backEnd applications",
+//     createdAt: "2022-08-03",
+//   },
+// ];
 
 function App() {
   const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  console.log(products)
 
   return (
     <div>
@@ -54,7 +57,7 @@ function App() {
         <NavBar />
         <div className="container max-w-screen-sm mx-auto p-4">
           <CategoryForm setCategories={setCategories} />
-          <ProductForm categories={categories} />
+          <ProductForm setProducts={setProducts} categories={categories} />
         </div>
       </div>
     </div>
--- src/Component/ProductForm.js
+++ src/Component/ProductForm.js
@@ -1,4 +1,34 @@
-const Product = ({ categories }) => {
+import { useState } from "react";
+
+const Product = ({ categories, setProducts }) => {
+  const [productFormData, setProductFormData] = useState({
+    title: "",
+    quantity: "",
+    category: "",
+  });
+
+  const changeHandler = (e) => {
+    setProductFormData({ ...productFormData, [e.target.name]: e.target.value });
+  };
+
+  const addNewProductHandler = (e) => {
+    e.preventDefault();
+
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        ...productFormData,
+        createdAt: new Date().toISOString(),
+        id: new Date().getTime(),
+      },
+    ]);
+    setProductFormData({
+      title: "",
+      quantity: "",
+      category: "",
+    });
+  };
+
   return (
     <div className="mb-6">
       <h2 className="text-xl text-slate-300 font-bold mb-2">Add New Product</h2>
@@ -8,8 +38,10 @@ const Product = ({ categories }) => {
             title
           </label>
           <input
+            onChange={(e) => changeHandler(e)}
+            value={productFormData.title}
             type="text"
-            name="product-title"
+            name="title"
             id="product-title"
             className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full md:w-auto"
           />
@@ -22,21 +54,22 @@ const Product = ({ categories }) => {
             quantity
           </label>
           <input
+            onChange={(e) => changeHandler(e)}
+            value={productFormData.quantity}
             type="number"
-            name="product-quantity"
+            name="quantity"
             id="product-quantity"
             className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full md:w-auto"
           />
         </div>
         <div className="mb-4">
-          <label
-            htmlFor="product-category"
-            className="block mb-1 text-slate-400"
-          >
+          <label htmlFor="category" className="block mb-1 text-slate-400">
             category
           </label>
           <select
-            name="product-category"
+            onChange={(e) => changeHandler(e)}
+            value={productFormData.category}
+            name="category"
             id="product-category"
             className="bg-transparent text-slate-400
                      rounded-xl w-full"
@@ -47,7 +80,7 @@ const Product = ({ categories }) => {
             {categories.map((category) => {
               return (
                 <option
-                  key={categories.id}
+                  key={category.id}
                   className="bg-slate-500 text-slate-300"
                 >
                   {category.title}
@@ -58,6 +91,7 @@ const Product = ({ categories }) => {
         </div>
         <div className="flex items-center justify-between gap-x-4">
           <button
+            onClick={(e) => addNewProductHandler(e)}
             id="add-new-product"
             className="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2"
           >
